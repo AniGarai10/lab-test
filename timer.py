a06f727aebbcf20c34f9a2c8b6b3de7c8ed18a28
AniGarai10 <37810943+AniGarai10@users.noreply.github.com>
2023-12-04 00:48:44 +0530

    updated timer.py

--- timer.py
+++ timer.py
@@ -3,6 +3,19 @@ Countdown Timer
 """
 
 import time
+import random
+
+
+def countdown(user_time):
+    while user_time >= 0:
+        mins, secs = divmod(user_time, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end='\r')
+        time.sleep(1)
+        user_time -= 1
+    print('Lift off!')
+
 
 if __name__ == "__main__":
-    print("Hi")
+    user_time = random.randrange(5, 60, 5)
+    countdown(user_time)
